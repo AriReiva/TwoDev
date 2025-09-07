76b2c22ae8ee4f9b5118fff681aa7ccdeb138a9d
AriReiva <rrardi0202@gmail.com>
2025-09-07 13:10:31 +0700

    Last Kali

--- app/src/main/java/com/example/lsp/SplashScreen.kt
+++ app/src/main/java/com/example/lsp/SplashScreen.kt
@@ -28,15 +28,6 @@ class SplashScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-//        window.decorView.setOnSystemUiVisibilityChangeListener { visibility ->
-//            if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
-//                setImmersiveMode()
-//            }
-//        }
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            startLockTask()
-//        }
         setContent {
             LSPTheme {
                 SplashContent {
@@ -46,22 +37,6 @@ class SplashScreen : ComponentActivity() {
             }
         }
     }
-//    override fun onWindowFocusChanged(hasFocus: Boolean) {
-//        super.onWindowFocusChanged(hasFocus)
-//        if (hasFocus) {
-//            setImmersiveMode()
-//        }
-//    }
-//
-//    private fun setImmersiveMode() {
-//        window.decorView.systemUiVisibility =
-//            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-//                    View.SYSTEM_UI_FLAG_FULLSCREEN or
-//                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-//                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-//                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//    }
 }
 
 @Composable
